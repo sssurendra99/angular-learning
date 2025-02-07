b516641a751a24f58a45222f35cd6b11b4c9cab2
Sumal Surendra <sumal.surendra@ascentic.se>
2025-02-08 00:58:37 +0530

    2/7 final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-learning';
+  title = 'Home Page';
 }
